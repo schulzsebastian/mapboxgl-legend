e55429335f2550fabeee1c82fb57ffa9118e895c
Sebastian Schulz <schulz.siwy@gmail.com>
2017-03-30 21:50:18 +0200

    overlays list as arg

--- mapbox-gl-legend.js
+++ mapbox-gl-legend.js
@@ -2,37 +2,30 @@ const className = 'mapboxgl-ctrl mapbox-gl-legend'
 
 class MapboxLegend {
     constructor(overlays) {
-        this._layers = {}
+        this._overlays = overlays
         this._container = document.createElement('div')
         this._container.className = `${className}`
-        //this._container.addEventListener('click', this._onClick.bind(this))
         this.addCSS()
     }
     onAdd(map) {
         this._map = map
-        this._map.on('data', this._onData.bind(this))
+        this._updateOverlays()
         return this._container
     }
-    _onClick(e) {
-        console.log(this._layers)
-    }
-    _onData(e) {
-        if(e.isSourceLoaded == true && e.sourceId != 'openmaptiles'){
-            if(Object.keys(this._layers).indexOf(e.source) < 0){
-                this._layers[e.sourceId] = e.source
-                this._updateLayers()
-            }
-        }
-    }
-    _updateLayers() {
+    _updateOverlays() {
         this._container.innerHTML = ''
         let ul = document.createElement('ul')
-        for(let layer in this._layers){
+        for(let layer of this._overlays){
             let li = document.createElement('li')
-            let li_text = document.createTextNode(layer)
+            let li_text = document.createTextNode(layer.id)
             let check = document.createElement("input");
             check.setAttribute("type", "checkbox")
-            check.id = layer
+            check.id = layer.id
+            this._map.on('load', () => {
+                if(this._map.getLayoutProperty('Point layer', 'visibility') == 'visible'){
+                    check.checked = true
+                }
+            })
             li.appendChild(check)
             li.appendChild(li_text)
             ul.appendChild(li)
