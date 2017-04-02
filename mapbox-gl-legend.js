4a7a1bfc33e4b52664c1a25ad10aa883b8ae6e74
Sebastian Schulz <schulz.siwy@gmail.com>
2017-04-03 00:34:15 +0200

    begin basemaps

--- mapbox-gl-legend.js
+++ mapbox-gl-legend.js
@@ -1,21 +1,29 @@
 const className = 'mapboxgl-ctrl mapboxgl-legend'
 const defaultOptions = {
     'order': 'qgis',
-    'customOrder': []
+    'overlaysOrder': [],
+    'basemapsOrder': []
 }
 
 class MapboxLegend {
-    constructor(overlays, options) {
-        // Overlays is an array of MapboxGL layers
+    constructor(overlays, basemaps, options) {
+        // Overlays is an object "layer name: MapboxGL layers"
         this._overlays = overlays
+        // Basemaps is an object "layer name: MapboxGL style"
+        this._basemaps = basemaps
         // Options is an optional parameter
         if(options == undefined) this._options = defaultOptions
         else this._options = options
-        // If the order is not specified - set object order
-        if(this._options.customOrder == undefined || this._options.customOrder.length == 0){
-            this._order = Object.keys(overlays).map((layerName) => overlays[layerName].id)
+        // If the order of overlays is not specified - set object order
+        if(this._options.overlaysOrder == undefined || this._options.overlaysOrder.length == 0){
+            this._overlaysOrder = Object.keys(overlays).map((layerName) => overlays[layerName].id)
         }
-        else this._order = this._options.customOrder
+        else this._overlaysOrder = this._options.overlaysOrder
+        // If the order of basemaps is not specified - set object order
+        if(this._options.basemapsOrder == undefined || this._options.basemapsOrder.length == 0){
+            this._basemapsOrder = Object.keys(basemaps)
+        }
+        else this._basemapsOrder = this._options.basemapsOrder
         // Creating legend container
         this._container = document.createElement('div')
         this._container.className = `${className}`
@@ -26,7 +34,7 @@ class MapboxLegend {
         // Define map object in class
         this._map = map
         // Run a function to create legend
-        this._updateOverlays()
+        this._updateLegend()
         // onAdd method requires return an DOM element
         return this._container
     }
@@ -36,24 +44,45 @@ class MapboxLegend {
     }
     changeOrder(newOrder) {
         // newOrder is an array of new order
-        this._order = newOrder
+        this._overlaysOrder = newOrder
         // Run a function to re-create legend
         this._updateOverlays()
     }
-    _updateOverlays() {
+    _updateLegend() {
         // Clearing legend
         this._container.innerHTML = ''
+        this._updateBasemaps()
+        this._updateOverlays()
+    }
+    _updateBasemaps() {
         // Creating a list
         let ul = document.createElement('ul')
+        ul.id = 'mapbox-gl-legend-basemaps'
+        // Creating list elements for each basemap
+        for(let layerName of this._basemapsOrder) ul.appendChild(this._createBasemap(layerName, this._basemaps[layerName]))
+        // Appending list to legend
+        this._container.appendChild(ul)
+        // Adding html line
+        this._container.appendChild(document.createElement('hr'))
+    }
+    _updateOverlays() {
+        // If list exists - clear it
+        if(document.getElementById('mapbox-gl-legend-overlays') != null){
+            let ul = document.getElementById('mapbox-gl-legend-overlays')
+            ul.parentNode.removeChild(ul)
+        }
+        // Creating a list
+        let ul = document.createElement('ul')
+        ul.id = 'mapbox-gl-legend-overlays'
         // Creating list elements for each layer
-        for(let layerId of this._order){
+        for(let layerId of this._overlaysOrder){
             for(let layerName in this._overlays){
                 if(this._overlays[layerName].id == layerId) ul.appendChild(this._createOverlay(layerName, this._overlays[layerName]))
             }
         }
         // Depends on the options set render order
-        if(this._options.order == 'qgis') for(let layerId of this._order.slice().reverse()) this._map.moveLayer(layerId)
-        else for(let layerId of this._order) this._map.moveLayer(layerId)
+        if(this._options.order == 'qgis') for(let layerId of this._overlaysOrder.slice().reverse()) this._map.moveLayer(layerId)
+        else for(let layerId of this._overlaysOrder) this._map.moveLayer(layerId)
         // Appending list to legend
         this._container.appendChild(ul)
     }
@@ -95,24 +124,40 @@ class MapboxLegend {
         // Return list element
         return li
     }
+    _createBasemap(layerName, layer) {
+        let li = document.createElement('li')
+        let radio = document.createElement('input')
+        radio.setAttribute('type', 'radio')
+        radio.setAttribute('name', 'basemaps')
+        radio.id = layerName
+        li.appendChild(radio)
+        li.appendChild(document.createTextNode(`${layerName}`))
+        radio.addEventListener('click', this._selectBasemap.bind(this))
+        return li
+    }
     _upLayer(e) {
         // Return if layer is the first one
         let layerId = e.target.getAttribute('id')
-        let idx = this._order.indexOf(layerId)
+        let idx = this._overlaysOrder.indexOf(layerId)
         if(idx == 0) return
         // Move layer to index - 1
-        this._order.splice(idx - 1, 0, this._order.splice(idx, 1)[0])
+        this._overlaysOrder.splice(idx - 1, 0, this._overlaysOrder.splice(idx, 1)[0])
         // Run a function to re-create legend
         this._updateOverlays()
     }
     _downLayer(e) {
         // Return if layer is the last one
         let layerId = e.target.getAttribute('id')
-        let idx = this._order.indexOf(layerId)
-        if(idx == this._order.length - 1) return
+        let idx = this._overlaysOrder.indexOf(layerId)
+        if(idx == this._overlaysOrder.length - 1) return
         // Move layer to index + 1
-        this._order.splice(idx + 1, 0, this._order.splice(idx, 1)[0])
+        this._overlaysOrder.splice(idx + 1, 0, this._overlaysOrder.splice(idx, 1)[0])
         // Run a function to re-create legend
+        this._updateOverlays()
+    }
+    _selectBasemap(e) {
+        this._map.setStyle(this._basemaps[e.target.getAttribute('id')])
+        //TODO: caching and load sources/layers from cache
         this._updateOverlays()
     }
     _toggleLayer(e) {
@@ -128,7 +173,7 @@ class MapboxLegend {
     _addCSS() {
         const css = document.createElement('style')
         let display
-        if(this._order.length > 0) display = 'in-line'
+        if(this._overlaysOrder.length > 0) display = 'in-line'
         else display = 'none'
         css.type = 'text/css'
         css.innerHTML = `
